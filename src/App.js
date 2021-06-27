e2339daa0528db190567d6b4cab5c0c17c183503
Rajkamal <rajkamal.smit@gmail.com>
2021-06-27 20:28:45 +0530

    icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { FaAdn } from "react-icons/fa";
+import { IconContext } from "react-icons";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IconContext.Provider value={{ color: "red", size: "100px" }}>
+      <div className="App">
+        <FaAdn />
+      </div>
+    </IconContext.Provider>
   );
 }
 
