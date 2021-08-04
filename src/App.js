eb7790adba120676b2ccb1e53bbc5ff20927f7ba
magesh android <magesh@globeshout.com>
2021-08-04 17:32:19 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import {useState} from 'react'
 
 function App() {
+  const [showPwd, setShowPwd]=useState(false)
+  const type=showPwd?'text':'password'
+  const pwdImg=showPwd?'https://icon-library.com/images/show-password-icon/show-password-icon-25.jpg':'https://static.thenounproject.com/png/506282-200.png'
   return (
     <div className="App">
-      <h2>Master</h2>
+      <div style={{display:'flex',alignItems:'center'}}>
+      <label>Username</label>{'  '}
+      <input 
+      type='text'/>
+
+</div>
+      <div style={{display:'flex',alignItems:'center'}}>
+      <label>Password</label>{'  '}
+      <input 
+      type={type}/>
+      <img width='30px'
+      style={{position:'absolute',left:'170px'}}
+      onClick={()=>setShowPwd(!showPwd)}
+      src={pwdImg}/>
+</div>
     </div>
   );
 }
